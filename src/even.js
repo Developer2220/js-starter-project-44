026ce1ecb26be6d4cdaaaac277c16c3047804e05
Pavel <web.js.ezh@gmail.com>
2023-07-25 00:10:23 +0300

    add src/even.js

---
+++ src/even.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+
+const evenGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const getRandomNumber = () => Math.floor(Math.random() * 100);
+  for (let i = 1; i <= 3; i += 1) {
+    const number = getRandomNumber();
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (number % 2 === 0) {
+      if (userAnswer === 'yes') {
+        console.log('Correct!');
+      } else {
+        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'yes'. /n Let's try again, ${name}!`);
+        return;
+      }
+    }
+    if (number % 2 !== 0) {
+      if (userAnswer === 'no') {
+        console.log('Correct!');
+      } else {
+        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was 'yes'. /n Let's try again, ${name}!`);
+        return;
+      }
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default evenGame;
