e2594b487f37614164a2a8a313d102a296c22216
Pavel <web.js.ezh@gmail.com>
2023-08-08 23:47:14 +0300

    fix even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,11 +1,8 @@
 const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-// const isEven = (x) => x % 2 === 0;
-
 const evenGame = () => {
   const getRandomNumber = () => Math.floor(Math.random() * 100);
   const question = getRandomNumber();
-  // const getCorrectAnswer = isEven(question) ? 'yes' : 'no';
 
   const correctAnswer = (x) => {
     let result;
